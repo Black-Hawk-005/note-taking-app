c4d988bd915989d07485f9978f4df22f3f205615
Black-Hawk-005 <premdanasekaran@gmail.com>
2025-12-11 10:56:03 +0530

    First commit of the project

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/api/notes", (req, res) => {
+  res.send("You got 5 notes");
+})
+
+app.listen(5001, () => {
+  console.log("Server started on port: http://localhost:5001");
+});
