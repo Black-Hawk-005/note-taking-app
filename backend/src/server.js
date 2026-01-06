6bef8e16a0b21b7149e484f33606a2c47c760f3b
Black-Hawk-005 <premdanasekaran@gmail.com>
2026-01-06 15:05:26 +0530

    Finished the backend part and implemented the rate limiter allowing 100 req per min

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,19 +2,28 @@ import express from "express"
 import notesRoutes from "./routes/notesRoutes.js"
 import {connectDB} from "./config/db.js"
 import dotenv from "dotenv"
+import rateLimiter from "./middleware/rateLimiter.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
 
-connectDB();
 
 //middle ware
 app.use(express.json());
+app.use(rateLimiter);
+
+
+app.use((req ,res, next) => {
+  console.log("We just got a new req");
+  next();
+});
 
 app.use("/api/notes", notesRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Server started on port: http://localhost:${PORT}`);
-});
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server started on port: http://localhost:${PORT}`);
+  });
+})
